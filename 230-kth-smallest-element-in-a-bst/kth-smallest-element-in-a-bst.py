0dabf92035673e5df59ef17a09bd48cde3389783
Rio <9rio10089@gmail.com>
2026-05-07 11:10:36 +0900

    Time: 0 ms (100.00%), Space: 22.12 MB (61.33%)

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # in-order solution
+        # 1. stack left nodes first until it's None
+        # 2. pop stack & go to the right node
+        # 3. repeat
+        # time: O(2n)=O(n) since each node will be processed exactly twice
+        # space: O(h) if the tree is balanced (h=logn), O(n) if the tree is skewed
+
+        node = root
+        stack = []
+
+        # while node: think of a tree with all nodes skewed to the right (there'll be a moment when the stack is empty, but there still are nodes unprocessed)
+        # while stack: when we've reached the left end or when node.right is None
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            k -= 1 # visit
+            if k == 0:
+                return node.val
+            node = node.right
+
+    def kthSmallestRecursive(self, root: Optional[TreeNode], k: int) -> int:
+        # return node.val in each step
+        # repeat k times, and then return the final answer
+
+        inorder = []
+        def dfs(node: Optional[TreeNode]): # define dfs function since we have to keep stack and inorder globally
+            if not node:
+                return
+            dfs(node.left)
+            inorder.append(node) # we don't have to define a stack since there'll be one automatically
+            dfs(node.right)
+
+        dfs(root)
+        return inorder[k-1].val
