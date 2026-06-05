b55747ed61483bd2f9dd47a1cbb31188b7a1106c
Rio <9rio10089@gmail.com>
2026-06-05 18:14:27 +0900

    875 Koko Eating Bananas

---
+++ 875-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # the ith pile has piles[i] bananas
+        # bananas-per-hour eating speed of k
+        # binary search between 1 and max(piles)
+        # min hour: when k = max(piles)
+        # max hour: when k = 1
+        # time: O(nlogm) where m is max(piles) and n is the length of piles
+        # space: O(1)
+
+        left, right = 1, max(piles)
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p / mid)
+
+            if hours <= h: # this condition includes "="
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
