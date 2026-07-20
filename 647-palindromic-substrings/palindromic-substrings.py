975c67d2bfea00e027c9dd79a2e315806970959a
Rio <9rio10089@gmail.com>
2026-07-20 20:35:38 +0900

    647 Palindromic Substrings

---
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count, count_odd, count_even = 0, 0, 0
+        def expand(left: int, right: int) -> int:
+            cnt_local = 0
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+                cnt_local += 1
+            return cnt_local
+
+        for i in range(len(s)):
+            count_odd = expand(i, i)
+            count_even = expand(i, i+1)
+
+            count += count_odd + count_even
+
+        return count
+
+    # time: O(n^2)
+    # space: O(1)
